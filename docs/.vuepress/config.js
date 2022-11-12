bad710122936c95af5eb20adb5978a0988b284f8
Hsiang Nianian <Hsiangnianian@outlook.com>
2022-11-12 10:28:58 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'ASR',  // 设置网站标题
   dest: './dist',    // 设置输出目录
-  base: '/', // 设置站点根路径
+  base: '/ASR/', // 设置站点根路径
   repo: 'https://github.com/cypress0522/ASR' // 添加 github 链接
 }
